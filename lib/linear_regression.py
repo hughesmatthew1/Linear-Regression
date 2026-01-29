25fa193c7f64d6f8ea5e8c4066fe22fab9c5a19d
hughesmatthew1 <m.hughes@ufl.edu>
2026-01-29 12:55:47 -0500

    First Commit

---
+++ lib/linear_regression.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Class for linear regression model
+class LinearRegression:
+    # Accepts 
+    def __init__():
+        pass
+
+    def fit():
+        pass
+
+    def predict():
+        pass
